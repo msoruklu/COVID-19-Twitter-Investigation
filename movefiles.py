3d44f5adb280e6cbaf010c0b0cdb2fc941695aff
Mutlu Soruklu <72261299+msoruklu@users.noreply.github.com>
2021-07-29 14:35:30 +0300

    Update movefiles.py

--- movefiles.py
+++ movefiles.py
@@ -2,8 +2,8 @@ import os
 from os import path
 import shutil
 
-src = "/cta/users/msoruklu/deneme/COVID-19-TweetIDs"
-dst = "/cta/users/msoruklu/deneme/COVID-19-TweetIDs/final_data"
+src = "./COVID-19-TweetIDs"
+dst = "./final_data"
 
 months = ["2020-03","2020-07","2020-10","2021-03","2021-06"]
 days = ["07","14","21","28"] #take days 7th,14th,21st and  28th from each month
@@ -13,7 +13,7 @@ sources = []
 for month in months: 
   for day in days:
     for hour in hours:
-      source = "/cta/users/msoruklu/deneme/COVID-19-TweetIDs/{}/coronavirus-tweet-id-{}-{}-{}.txt".format(month,month,day,hour)
+      source = "./{}/coronavirus-tweet-id-{}-{}-{}.txt".format(month,month,day,hour)
       sources.append(source)
 
 
@@ -21,4 +21,4 @@ for f in sources:
     shutil.copy(f, dst)
 
 #also add 1 day from 2020 January since it starts from 21st of Junuary, take 25t of that month
-shutil.copy("/cta/users/msoruklu/deneme/COVID-19-TweetIDs/2020-01/coronavirus-tweet-id-2020-01-25-15.txt", dst)
+shutil.copy("./2020-01/coronavirus-tweet-id-2020-01-25-15.txt", dst)
